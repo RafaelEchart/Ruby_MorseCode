caf331a2cedab5dd2e1c32a4acea3631f52ac103
Juan Carlos <jchernandez827@gmail.com>
2022-01-19 15:10:47 -0500

    linters fix

--- morse_code.rb
+++ morse_code.rb
@@ -1,60 +1,37 @@
-$morse_hash = { 
-  A: ".-", 
-  B: "-...", 
-  C: "-.-.", 
-  D: "-..",
-  E: ".",
-  F: "..-.",
-  G: "--.",
-  H: "....",
-  I: "..",
-  J: ".---",
-  K: "-.-",
-  L: ".-..",
-  M: "--",
-  N: "-.",
-  O: "---",
-  P: ".--.",
-  Q: "--.-",
-  R: ".-.",
-  S: "...",
-  T: "-",
-  U: "..-",
-  V: "...-",
-  W: ".--",
-  X: "-..-",
-  Y: "-.--",
-  Z: "--.."
-}
+def morse_hash
+  { A: '.-', B: '-...', C: '-.-.', D: '-..', E: '.', F: '..-.', G: '--.', H: '....',
+    I: '..', J: '.---', K: '-.-', L: '.-..', M: '--', N: '-.', O: '---', P: '.--.',
+    Q: '--.-', R: '.-.', S: '...', T: '-', U: '..-', V: '...-', W: '.--', X: '-..-', Y: '-.--', Z: '--..' }
+end
 
 def decode_char(string)
-  $morse_hash.each { |key, value| 
+  morse_hash.each do |key, value|
     if value == string
       letter = key.to_s
       return letter
     end
-  }
+  end
 end
 
 def decode_word(string)
-  split_letters = string.split(' ')
-  word = ""
-  split_letters.each {|el| 
+  split_letters = string.split
+  word = ''
+  split_letters.each do |el|
     word += decode_char(el)
-  }
-  return word
+  end
+  word
 end
 
 def decode_sentense(string)
-  newString = string.gsub("/", ' ')
-  split_word = newString.split('   ')
+  new_string = string.gsub('/', ' ')
+  split_word = new_string.split('  ')
   sentense = ''
-  split_word.each {|word|
-    sentense += decode_word(word) + ' '
-  }
-  return sentense
-end  
+  split_word.each do |word|
+    sentense += "#{decode_word(word)} "
+  end
+  sentense
+end
 
-puts decode_char(".-")
-puts decode_word("-- -.--")
+puts decode_char('.-')
+puts decode_word('-- -.--')
 puts decode_sentense('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
