d6dd4cccf49b228d835eb273a2361a1f6641814b
Juan Carlos <jchernandez827@gmail.com>
2022-01-19 12:11:38 -0500

    Add a morse_hash, Add decode_char method

---
+++ morse_code.rb
@@ -0,0 +1,39 @@
+@morse_hash = { 
+  A: ".-", 
+  B: "-...", 
+  C: "-.-.", 
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--.."
+}
+
+def decode_char(string)
+  @morse_hash.each { |key, value| 
+    if value == string
+      letter = key.to_s
+      puts letter
+    end
+  }
+end
+
+decode_char(".-")
